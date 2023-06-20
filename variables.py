b48d33d9ccfb310179d0ea273dcad8c96a0ca9e3
sanch046 <alex.sanchezcano@wur.nl>
2023-06-20 20:12:30 +0200

    change project name

--- variables.py
+++ variables.py
@@ -2,11 +2,11 @@
 Store project variables
 '''
 
-CAZY_DATA = 'lustre/maquinas/Data'
+CAZY_DATA = 'lustre/ML-on-LPMOs/Data'
 CAZY_EXPANDED = f'{CAZY_DATA}/CAZy_expanded'
 AF_FILES = f'{CAZY_DATA}/AF_files'
 AF_HIS1 = f'{CAZY_DATA}/AF_His1'
 AF_CORE = f'{CAZY_DATA}/AF_core'
 AF_ACTIVE_SITE = f'{CAZY_DATA}/AF_active_site'
-PLOTS = f'lustre/maquinas/Plots'
 FASTA = f'{CAZY_DATA}/FASTA'
+PLOTS = f'lustre/ML-on-LPMOs/Plots'
