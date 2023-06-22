5587a7d43f85e650984c7f0fb49aca0a03780d2c
sanch046 <alex.sanchezcano@wur.nl>
2023-06-22 10:02:08 +0200

    separate calculate matrix and plotting

--- plot_sequence_dendrogram.py
+++ plot_sequence_dendrogram.py
@@ -1,50 +1,27 @@
-import os
+import pickle
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from tqdm import tqdm
-from Bio import Align
-from parse_structures import AlphaFoldStructure
-from variables import FASTA, PLOTS, SEQUENCE_CLUSTERING, CAZY_EXPANDED
+from variables import SEQUENCE_CLUSTERING, CAZY_DATA, FASTA, PLOTS
 
-print('started')
+# Import matrix
+matrix = np.load(f'{SEQUENCE_CLUSTERING}/his1_matrix.npy')
 
-# List all sequences
+# Get AlphaFold IDs 
 with open(f'{FASTA}/His1.fasta', 'r') as file:
     sequences = file.read().split('\n\n')[:-1]
-    sequences = [fasta.split('\n')[1] for fasta in sequences]
     ids = [fasta[1:].split('\n')[0] for fasta in sequences]
 
-# Configure pairwise alignemnt
-aligner = Align.PairwiseAligner()
-aligner.substitution_matrix = Align.substitution_matrices.load("PAM70")
-aligner.open_gap_score = -4
-aligner.extend_gap_score = -0.5
+# AlphaFold IDs -> corresponding families
+with open(f'{CAZY_DATA}/uniprot_family.pkl', 'rb') as dic:
+    dic = pickle.load(dic)
 
-# Compute pairwise alignment -> O(nlog(n))
-n_seqs = len(sequences)
-matrix = np.zeros([n_seqs, n_seqs])
-for i in tqdm(range(n_seqs)):
-    for j in range(i, n_seqs):
-        score = aligner.align(sequences[i], sequences[j]).score
-        matrix[i][j] = int(score)
 
-# Triangular matrix -> full matrix
-X = matrix + matrix.T - np.diag(np.diag(matrix))
-np.save(f'{SEQUENCE_CLUSTERING}/his1_matrix', X)
-
-# Assign color to family
-df = pd.read_pickle(f'{CAZY_EXPANDED}/AA_filtered')
+# Assign color to each family
 families = ['AA0', 'AA9', 'AA10', 'AA11', 'AA13', 'AA14', 'AA15', 'AA16', 'AA17']
 families_colors = dict(zip(families, sns.color_palette()))
-joined_df = pd.merge(
-    left = df,
-    right = pd.DataFrame({'UniProt' : ids}),
-    on = 'UniProt'
-).drop_duplicates('UniProt')
-
-colors = joined_df['Family'].map(families_colors).to_numpy()
+colors = [families_colors[dic[id]] for id in ids]
 
 # Plot
-heatmap = sns.clustermap(X, row_cluster = False, column_colors = colors)
+heatmap = sns.clustermap(matrix, col_colors = colors, row_colors = colors)
 heatmap.savefig(f'{PLOTS}/sequence_heatmap.png', transparent=True)
