234a415750364af7e3cf753a8a2481d23964b988
sanch046 <alex.sanchezcano@wur.nl>
2023-06-22 10:00:12 +0200

    add structure_sequence and convert to absolute paths

--- variables.py
+++ variables.py
@@ -2,12 +2,13 @@
 Store project variables
 '''
 
-CAZY_DATA = 'lustre/ML-on-LPMOs/Data'
+CAZY_DATA = '/home/sanch046/lustre/ML-on-LPMOs/Data'
 CAZY_EXPANDED = f'{CAZY_DATA}/CAZy_expanded'
 AF_FILES = f'{CAZY_DATA}/AF_files'
 AF_HIS1 = f'{CAZY_DATA}/AF_His1'
 AF_CORE = f'{CAZY_DATA}/AF_core'
 AF_ACTIVE_SITE = f'{CAZY_DATA}/AF_active_site'
-SEQUENCE_CLUSTERING = f'{CAZY_DATA}/Sequence_clustering'
 FASTA = f'{CAZY_DATA}/FASTA'
-PLOTS = f'lustre/ML-on-LPMOs/Plots'
+SEQUENCE_CLUSTERING = f'{CAZY_DATA}/Sequence_clustering'
+STRUCTURE_CLUSTERING = f'{CAZY_DATA}/Structure_clustering'
+PLOTS = f'/home/sanch046/lustre/ML-on-LPMOs/Plots'
