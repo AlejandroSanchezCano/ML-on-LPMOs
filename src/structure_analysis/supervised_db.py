1bdadacab143c2d4152b82e89567c7758ca113c4
sanch046 <alex.sanchezcano@wur.nl>
2023-07-04 09:34:58 +0200

    create database supervised

---
+++ src/structure_analysis/supervised_db.py
@@ -0,0 +1,106 @@
+db = [
+    # AA9
+    ('AA9', 'AN3046', 'A0A1U8QMG7', 'C1', ('cellulose', 'xyloglucan')),
+    ('AA9', 'CvAA9A', 'A0A223GEC9', 'C1/C4', ('cellulose', 'xyloglucan', 'glucomannan')),
+    ('AA9', 'FgLPMO9A', 'I1REU9', 'C1/C4', ('cellulose, xyloglucan')),
+    ('AA9', 'GtLPMO9A-2', 'A0A1C9ZMC5', 'C1/C4', ('cellulose', 'xyloglucan', 'glucomannan')),
+    ('AA9', 'GcLPMO9A', 'A0A0J9XKT0', 'C1/C4', ('cellulose', 'xyloglucan')),
+    ('AA9', 'GcLPMO9B', 'A0A0J9XL55', 'C1/C4', ('cellulose', 'xyloglucan')),
+    ('AA9', 'HiLPMO9B', 'W4KMP1', 'C1', ('cellulose')),
+    ('AA9', 'HiLPMO9H', 'W4K498', 'C1', ('cellulose')),
+    ('AA9', 'HiLPMO9I', 'W4K8M0', 'C4', ('cellulose', 'glucomannan')),
+    ('AA9', 'Ls(AA9)A', 'A0A0S2GKZ1', 'C4', ('cellulose', 'xyloglucan', 'glucomannan')),
+    ('AA9', 'MtLPMO9A', 'A0A0H4K9X4', 'C1/C4', ('cellulose, xyloglucan')),
+    ('AA9', 'MtLPMO9B', 'A0A1C9CXI1', 'C1', ('cellulose')),
+    ('AA9', 'MtLPMO9C', 'A0A1C9CXI0', 'C4', ('cellulose, xyloglucan')),
+    ('AA9', 'MtLPMO9D', 'A0A218MJF1', 'C1', ('cellulose')),
+    ('AA9', 'MtLPMO9E', '', 'C4', ('cellulose, xyloglucan')), #idk
+    ('AA9', 'MtLPMO9F', '', 'C4', ('cellulose')),             #idk
+    ('AA9', 'MtLPMO9G', '', 'C4', ('cellulose')),             #idk
+    ('AA9', 'MtLPMO9', 'G2Q9T3', 'C1/C4', ('cellulose')),
+    ('AA9', 'MYCTH_112089', 'G2QI82', 'C1', ('cellulose')),
+    ('AA9', 'NCU00836', 'Q7SCJ5', 'C1', ('cellulose')),
+    ('AA9', 'NcLPMO9D', 'Q1K8B6', 'C4', ('cellulose')),
+    ('AA9', 'NcLPMO9J', 'Q7SHD9', 'C1', ('cellulose')),
+    ('AA9', 'NcLPMO9A', 'Q7S439', 'C4', ('cellulose')),
+    ('AA9', 'NcLPMO9C', 'Q7SHI8', 'C4', ('cellulose', 'xyloglucan', 'glucomannan')),
+    ('AA9', 'NcLPMO9F', 'Q1K4Q1', 'C1', ('cellulose')),
+    ('AA9', 'NCU07760', 'Q7S111', 'C1/C4', ('cellulose')),
+    ('AA9', 'NcLPMO9M', 'Q7SA19', 'C1', ('cellulose')),
+    ('AA9', 'NCU08760', 'Q7RWN7', 'C1', ('cellulose')),
+    ('AA9', 'PsLPMO9A', 'A0A167KNZ4', 'C1/C4', ('cellulose')),
+    ('AA9', 'PsLPMO9B', 'A0A167KNY8', 'C4', ('cellulose')),
+    ('AA9', 'PcLPMO9D', 'H1AE14', 'C1', ('cellulose')),
+    ('AA9', 'PaLPMO9A', 'B2B629', 'C1/C4', ('cellulose')),
+    ('AA9', 'PaLPMO9B', 'B2AVF1', 'C1/C4', ('cellulose')),
+    ('AA9', 'PaLPMO9E', 'B2ATL7', 'C1', ('cellulose')),
+    ('AA9', 'PaLPMO9H', 'B2ADG1', 'C1/C4', ('cellulose', 'xyloglucan', 'glucomannan')),
+    ('AA9', 'TaLPMO9A', 'G3XAP7', 'C1/C4', ('cellulose')),
+    ('AA9', 'HjLPMO9A', 'G0R6T8', 'C1/C4', ('cellulose')),
+    ('AA9', 'TtLPMO9E', 'D0VWZ9', 'C1', ('celllulose')),
+
+    # AA10 idk if all chitin-degrading LPMOs are C1
+    ('AA10', 'AsLPMO10A', 'B6EQB6', 'C1', ('chitin')),
+    ('AA10', 'AsLPMO10B', 'B6EQJ6', 'C1', ('chitin')),
+    ('AA10', 'BaAA10A', 'E1UUV3', 'C1', ('chitin')),
+    ('AA10', 'BlAA10A', 'D0EW67', 'C1', ('chitin')),
+    ('AA10', 'BtLPMO10A-FL', 'D0EW65', 'C1', ('chitin')),
+    ('AA10', 'BtLPMO10A', 'A0A0C5K362', 'C1', ('chitin')),
+    ('AA10', 'CfiLPMO10', 'F4H6A3', 'C1/C4', ('cellulose')),
+    ('AA10', 'CflaLPMO10A', 'D5UGB1', 'C1', ('cellulose')),
+    ('AA10', 'CflaLPMO10B', 'D5UGA8', 'C1/C4', ('cellulose')),
+    ('AA10', 'CflaLPMO10C', 'D5UH31', 'C1/C4', ('cellulose')),
+    ('AA10', 'CflaLPMO10D', 'D5UHY1', 'C1', ('chitin')),
+    ('AA10', 'CjLPMO10A', 'B3PJ79', 'C1', ('chitin')),
+    ('AA10', 'CjLPMO10B', 'B3PDT6', 'C1', ('cellulose')),
+    ('AA10', 'EfAA10A', 'Q838S1', 'C1', ('chitin')),
+    ('AA10', 'HcAA10-2', 'Q2SNS3', 'C1', ('cellulose')),
+    ('AA10', 'JdLPMO10A', 'C7R4I0', 'C1', ('chitin')),
+    ('AA10', 'KpLPMO10A', 'A0A285DJD7', 'C1/C4', ('cellulose', 'chitin', 'xylan')), #C1 on chitin
+    ('AA10', 'LmLPMO10', 'Q8Y4H4', 'C1', ('chitin')),
+    ('AA10', 'plu2352', 'Q7N4I5', 'C1', ('chitin')),
+    ('AA10', 'SmAA10A', 'O83009', 'C1', ('chitin')),
+    ('AA10', '22kDa protein', 'Q59930', 'C1', ('chitin')),
+    ('AA10', 'SamLPMO10B', 'A3KIM2', 'C1', ('chitin')),
+    ('AA10', 'SamLPMO10C', 'A3KKC4', 'C1', ('cellulose')),
+    ('AA10', 'ScLPMO10A', 'Q9S296', 'C1', ('chitin')),
+    ('AA10', 'ScLPMO10B', 'Q9RJC1', 'C1/C4', ('cellulose')),
+    ('AA10', 'ScLPMO10C', 'Q9RJY2', 'C1', ('cellulose')),
+    ('AA10', 'SgLPMO10A', 'B1VNK5', 'C1', ('cellulose', 'chitin')),
+    ('AA10', 'SgLPMO10F', 'B1VN59', 'C1', ('chitin')),
+    ('AA10', 'SliLPMO10E', 'A0A7U9DRA2', 'C1', ('chitin')),
+    ('AA10', 'TtAA10A', 'C5BKQ9', 'C1/C4', ('cellulose')),
+    ('AA10', 'TfLPMO10A', 'Q47QG3', 'C1/C4', ('cellulose')),
+    ('AA10', 'TfAA10B', 'Q47PB9', 'C1', ('cellulose')),
+    ('AA10', 'VcAA10B', 'A9Y370', 'C1', ('chitin')),
+    ('AA10', 'LpsAA10A', 'A0A8K1XN59', 'C1', ('cellulose')),
+    ('AA10', 'lytic chitin monooxygenase', 'W5QLL4', 'C1', ('chitin')),
+    ('AA10', 'fusolin (ACV034)', 'O70709', 'C1', ('chitin')),
+
+    # AA11
+    ('AA11', 'AfAA11A', 'B0XZD3', 'C1', ('chitin')),
+    ('AA11', 'A0AA11', 'Q2UA85', 'C1', ('chitin')),
+
+    # AA13
+    ('AA13', 'AnAA13', 'A0A1U8QN05', 'C1', ('chitin')),
+    ('AA13', 'A0AA13', 'Q2U8Y3', 'C1', ('chitin')),
+    ('AA13', 'AtLPMO13A', 'Q0CGA6', 'C1', ('chitin')),
+    ('AA13', 'NcAA13', 'Q6MWQ3', 'C1', ('chitin')),
+
+    # AA14
+    ('AA14', 'PcAA14A', 'A0A2I6QB00', 'C1', ('xylan')),
+    ('AA14', 'PcAA14B', 'A0A2I6QAZ5', 'C1', ('xylan')),
+
+    # AA15
+    ('AA15', 'AaAA15', 'W4GRV7', 'C1', ('chitin')),
+    ('AA15', 'TdAA15A', 'A0A2N8U5K6', 'C1', ('chitin', 'cellulose')),
+    ('AA15', 'TdAA15B', 'A0A2N8U5I8', 'C1', ('chitin', 'cellulose')),
+
+    # AA16
+    ('AA16', 'AaAA16', 'A0A1L9X7U6', 'C1', ('cellulose')),
+
+    # AA17
+    ('AA17', 'PiAA17A', 'A0A833WPX8', 'C4', ('homogalacturonan')),
+    ('AA17', 'PiAA17A', 'D0N2F6', 'C4', ('homogalacturonan')),
+    ('AA17', 'PiAA17C', 'D0N2F7', 'C4', ('homogalacturonan')),
+]
