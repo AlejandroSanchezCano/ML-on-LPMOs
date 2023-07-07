a377636369cfe0b5f14f134b298cbd9482e8570a
sanch046 <alex.sanchezcano@wur.nl>
2023-07-07 13:03:16 +0200

    add binary annotations

--- src/sequence_analysis/plot_dendrogram.py
+++ src/sequence_analysis/plot_dendrogram.py
@@ -68,8 +68,8 @@ print(newick)
 with open(f'{SEQUENCE_CLUSTERING}/core_tree.txt', 'w') as tree_file:
     tree_file.write(newick)
 
-#  Write annotation file
-with open(f'{SEQUENCE_CLUSTERING}/core_annotations.txt', 'w') as annotations_file:
+#  Write annotation color file
+with open(f'{SEQUENCE_CLUSTERING}/core_annotations_color.txt', 'w') as annotations_file:
     settings = '''
 DATASET_COLORSTRIP
 #lines starting with a hash are comments and ignored during parsing
@@ -111,3 +111,96 @@ DATA
 
     for id in ids:
         annotations_file.write(f'{id} {family_color[dic[id]]}\n')
+
+
+#  Write annotation binary file
+from variables import CAZY_EXPANDED, SUPERVISED
+
+df_labels = pd.read_pickle(f'{SUPERVISED}/regioselectivity_substratespecificity_db.pkl')
+
+def f(regio, substrate):
+    if regio == 'C1':
+        c1 = '1'
+        c4 = '0'
+    elif regio == 'C4':
+        c1 = '0'
+        c4 = '1'
+    else:
+        c1, c4 = '1', '1'
+    
+    if 'chitin' in substrate:
+        s = '0'
+    elif 'cellulose' in substrate:
+        s = '1'
+    else:
+        s = '-1'
+    return c1, c4, s
+
+l = df_labels.apply(lambda x: f(x.Regioselectivity, x['Substrate specificity']), axis = 1)
+d = dict(zip(df_labels['UniProt'], l))
+
+with open(f'{SEQUENCE_CLUSTERING}/core_annotations_binary.txt', 'w') as annotations_file:
+    settings = '''DATASET_BINARY
+#select the separator which is used to delimit the data below (TAB,SPACE or COMMA).This separator must be used throught this file (except in the SEPARATOR line, which uses space).
+
+#SEPARATOR TAB
+#SEPARATOR SPACE
+SEPARATOR COMMA
+
+#label is used in the legend table (can be changed later)
+DATASET_LABEL,binary_data
+
+#dataset color (can be changed later)
+COLOR,#ff0000
+
+#Binary datasets can contain one or more values for each node. Each value will be represented by a symbol (defined in FIELD_SHAPES) with corresponding color and label (from FIELD_COLORS and FIELD_LABELS). Possible values (defined under DATA below) for each node are 1 (filled shapes), 0 (empty shapes) and -1 (completely ommited).
+
+#define colors for each individual field column (if not defined all symbols will use the main dataset color, defined in COLOR)
+#shapes for each field column; possible choices are
+#1: rectangle 
+#2: circle
+#3: star
+#4: right pointing triangle
+#5: left pointing triangle
+FIELD_LABELS,rl0,rl1,rl2
+FIELD_COLORS,#000,#000,#66f
+FIELD_SHAPES,4,5,3
+
+#all other optional settings can be set or changed later in the web interface (under 'Datasets' tab)
+
+#show internal values; if set, values associated to internal nodes will be displayed even if these nodes are not collapsed. It could cause overlapping in the dataset display.
+SHOW_INTERNAL,1
+
+#left margin, used to increase/decrease the spacing to the next dataset. Can be negative, causing datasets to overlap.
+MARGIN,0
+
+#symbol height factor; Default symbol height will be slightly less than the available space between leaves, but you can set a multiplication factor here to increase/decrease it (values from 0 to 1 will decrease it, values above 1 will increase it)
+HEIGHT_FACTOR,1
+
+#increase/decrease the spacing between individual levels, when there is more than one binary level defined 
+SYMBOL_SPACING,10
+
+#Internal tree nodes can be specified using IDs directly, or using the 'last common ancestor' method described in iTOL help pages
+#Actual data follows after the "DATA" keyword
+DATA
+
+#Example dataset with 4 columns (circle, left triangle, right triangle and rectangle):
+#FIELD_SHAPES,2,4,5,1
+#FIELD_LABELS,f1,f2,f3,f4
+#FIELD_COLORS,#ff0000,#00ff00,#ffff00,#0000ff
+#DATA
+#node 9606 will have a filled circle, empty left triangle, nothing in the 3rd column and an empty rectangle
+#9606,1,0,-1,0
+'''
+
+
+    annotations_file.write(settings)
+
+
+    for id in ids:
+        if id  in d:
+            labels = ','.join(d[id])
+            annotations_file.write(f'{id},{labels}\n')
+        else:
+            annotations_file.write(f'{id},-1,-1,-1\n')
+        
