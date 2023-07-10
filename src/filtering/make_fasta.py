a19d64796e255842e5b4d249175d09ca9965c675
sanch046 <alex.sanchezcano@wur.nl>
2023-07-10 14:21:40 +0200

    fasta for all proteins

--- src/filtering/make_fasta.py
+++ src/filtering/make_fasta.py
@@ -1,17 +1,17 @@
 import os
 import argparse
 from tqdm import tqdm
-from variables import AF_CORE, FASTA
+from variables import AF_FILES, FASTA
 from parse_structures import AlphaFoldStructure
 
 # Get protein sequence in FASTA format
 fastas = []
-for protein in tqdm(os.listdir(AF_CORE)):
-    fasta = AlphaFoldStructure(f'{AF_CORE}/{protein}').fasta()
+for protein in tqdm(os.listdir(AF_FILES)):
+    fasta = AlphaFoldStructure(f'{AF_FILES}/{protein}').fasta()
     fastas.append(fasta)
 
 # Make FASTA file
-with open(f'{FASTA}/core.fasta', 'w') as file:
+with open(f'{FASTA}/files.fasta', 'w') as file:
     for seq in fastas:
         file.write(seq)
 
