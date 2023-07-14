20012b40889ddeee00c2304916d45fbc19b97183
sanch046 <alex.sanchezcano@wur.nl>
2023-07-14 22:07:20 +0200

    rename and add comments

---
+++ src/download_data/make_uniprot_family.py
@@ -0,0 +1,25 @@
+'''
+Make uniprot : family dictionary
+
+Make a dictionary with key the UniProt IDs and values the LPMO families they
+correspond to.
+
+Functions
+---------
+main()
+'''
+
+import pickle
+import pandas as pd
+from ..config.config_parser import config
+
+def main():
+    '''Program flow.'''
+    df = pd.read_pickle(f"{config['CAZy_expanded']}/all.pkl")
+    dic = dict(zip(df['UniProt'], df['Family']))
+
+    with open(f"{config['data']}/uniprot_family.pkl", 'wb') as file:
+        pickle.dump(dic, file)
+
+if __name__ == '__main__':
+    main()
